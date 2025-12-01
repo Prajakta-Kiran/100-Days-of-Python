3a3408b52773f69792667272379baf38e0230f28
Prajakta Kshirsagar <prajaktakc4353@gmail.com>
2025-12-01 09:03:46 -0500

    Adding batch name generator project

---
+++ band_name_generator.py
@@ -0,0 +1,9 @@
+print("Welcome to Band Name Generator!")
+
+user_name = input("Hi,Enter your name\n")
+user_city = input("Which city you grew up?\n")
+user_pet_name = input("What is name of your pet?\n")
+
+print("Here is your Band Name,"+user_name+":", user_city + user_pet_name)
+      
+      
